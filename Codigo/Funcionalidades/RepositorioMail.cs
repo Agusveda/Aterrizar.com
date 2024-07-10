dee7a43dfb2503df509688fa6e0d7f78dd9b3f12
Facundo <124022262+FacundoDorrego@users.noreply.github.com>
2024-07-10 17:08:55 -0300

    Armado de mail

--- Codigo/Funcionalidades/RepositorioMail.cs
+++ Codigo/Funcionalidades/RepositorioMail.cs
@@ -23,7 +23,30 @@ namespace Funcionalidades
 
         }
 
-        
+        public void armarCorreo(string destinatario, string asunto, string mensaje)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("dotcomaterrizar@gmail.com");
+            email.To.Add(destinatario);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            email.Body = "<h1>¡Hola!</h1><p>Te enviamos este correo para informarte que tu informacion ha sido guardada.</p><p>¡Pronto te contactaremos!</p>";
+            //email.Body = mensaje;
+        }
+
+        public void enviarCorreo()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
 
 
 
