5a7db2f6f631643659aa269165fb41d3b816370d
Facundo <124022262+FacundoDorrego@users.noreply.github.com>
2024-07-10 17:08:24 -0300

    Servidor del mail

---
+++ Codigo/Funcionalidades/RepositorioMail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionalidades
+{
+    public class RepositorioMail
+    {
+        private MailMessage email;
+        private SmtpClient server;
+        public void EmailService()
+        {                
+            server = new SmtpClient();
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+            server.Credentials = new System.Net.NetworkCredential("dotcomaterrizar@gmail.com", "ltor uzlk cnqo wqhz ");
+
+
+        }
+
+        
+
+
+
+
+    }
+}
